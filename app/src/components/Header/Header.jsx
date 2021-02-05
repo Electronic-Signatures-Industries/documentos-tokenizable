70626e31d19b5d89aef5f52434b8c060c5bd1a08
JoelDeTrinidad <joeldetrinidad@gmail.com>
2021-02-04 20:46:41 -0600

    Adding header

--- app/src/components/Header/Header.jsx
+++ app/src/components/Header/Header.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import i18n from 'i18next';
 
 import './header.scss';
 
 const Header = () => {
 	return (
-		<header id="main-header" className="main-header div-header">
-			{i18n.t('header_title')}
-		</header>
+		<div className="root">
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" className="title">
+						{i18n.t('header_title')}
+					</Typography>
+					<Button color="inherit">{i18n.t('header_conect')}</Button>
+				</Toolbar>
+			</AppBar>
+		</div>
 	)
     
 }
--- app/src/services/i18n.js
+++ app/src/services/i18n.js
@@ -1,18 +1,22 @@
 import i18n from 'i18next';
 import translationES from '../assets/locales/es/isotope.json';
+import translationEN from '../assets/locales/en/isotope.json';
 
 const userLanguage = () => navigator.language;
 
 const resources = {
 	es: {
 		translation: translationES,
+	},
+	en: {
+		translation: translationEN,
 	}
 };
 
 i18n.init({
 	resources,
 	lng: userLanguage(),
-	fallbackLng: ['es'],
+	fallbackLng: ['es', 'en'],
 });
 
 export default i18n;
