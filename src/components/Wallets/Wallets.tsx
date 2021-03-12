b457b2b7590dc619ceb3ef6cdeda6ac6fef1b6cd
Patricia Carrillo <patriciapilarcarrillo@hotmail.com>
2021-03-12 17:16:30 -0600

    cambiando el root del proyecto

---
+++ src/components/Wallets/Wallets.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import './wallets.scss';
+class Wallets extends React.Component<WalletProps,any>{
+	render(){
+		const Icon = this.props.icon;
+	return (
+		<Grid className="wallet-border" component="span">
+			<Typography component="span">{this.props.title}</Typography>
+			<Icon />
+			</Grid>
+	)
+	}
+}
+
+
+type WalletProps = {
+	title: string,
+	icon: any
+}
+
+export default Wallets;
