9efc0c9b4cb7cd6fde498ba3d4dfc3c7f7143a40
Rogelio Morrell <molekilla@gmail.com>
2021-03-13 09:46:57 -0500

    sidebar to tsx

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
+import Home from '@material-ui/icons/Home'
+import GroupIcon from '@material-ui/icons/Group'
+import AssignmentIcon from '@material-ui/icons/Assignment'
+import i18n from 'i18next'
+
+import './sideBar.scss'
+
+class SideBar extends React.Component<SideBarProps, any> {
+  render() {
+    const { isSlide } = this.props
+    const slide = isSlide ? 'slide' : ''
+    const pathname = location.pathname
+    return (
+      <div className={`${slide} container-sidebar`}>
+        <ul>
+          <NavLink to="/">
+            <li className={slide ? 'select-list' : ''}>
+              <div className={pathname === '/' ? 'selected' : ''}></div>
+              <Home className="icon-color" />
+              {!slide ? <span>{i18n.t('sidebar_home')}</span> : null}
+            </li>
+          </NavLink>
+          <NavLink to="/provider">
+            <li className={slide ? 'select-list' : ''}>
+              <div className={pathname === '/provider' ? 'selected' : ''}></div>
+              <GroupIcon className="icon-color" />
+              {!slide ? <span>{i18n.t('sidebar_provider')}</span> : null}
+            </li>
+          </NavLink>
+          <NavLink to="/request">
+            <li className={slide ? 'select-list' : ''}>
+              <div className={pathname === '/request' ? 'selected' : ''}></div>
+              <AssignmentIcon className="icon-color" />
+              {!slide ? <span>{i18n.t('sidebar_request')}</span> : null}
+            </li>
+          </NavLink>
+        </ul>
+      </div>
+    )
+  }
+}
+
+type SideBarProps = {
+  isSlide: boolean
+}
+
+export default SideBar
