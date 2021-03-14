67a30f5faf09f4c11b80b2971e23f6c952dbcbdf
Rogelio Morrell <molekilla@gmail.com>
2021-03-14 12:33:10 -0500

    migrate layout to tsx

--- src/containers/Layout/Layout.tsx
+++ src/containers/Layout/Layout.tsx
@@ -1,39 +1,37 @@
-import React, { useState }  from 'react'
-import PropTypes from 'prop-types';
-import {Container, Row, Col } from 'reactstrap';
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { Container, Row, Col } from 'reactstrap'
 
-import Header from '../../components/Header/Header';
-import SideBar from '../../components/SideBar/SideBar';
-import './layout.scss';
+import Header from '../../components/Header/Header'
+import SideBar from '../../components/SideBar/SideBar'
+import './layout.scss'
 
-const Layout = ({children}) => {  
-	const [isSlide, setIsSlide] = useState(false);
-	return (
-		<div >         
-			<Header setIsSlide={setIsSlide} isSlide={isSlide} />
-			<Row className="row-layout">
-				<Col md={isSlide ? '2' : '3'} xs="4" className="col-sidebar">
-					<SideBar isSlide={isSlide} />
-				</Col>
-				<Col xs="8" md={isSlide ? '10' : '9'}>
-					<main>
-						<Container>
-							{children}
-						</Container>
-					</main>
-					<div>
-						<Container>
-						</Container>
-					</div>
-				</Col>
-			</Row>
-			
-		</div>
-	)
+class Layout extends React.Component<LayoutProps, any> {
+  render() {
+    const [isSlide, setIsSlide] = useState(false)
+    return (
+      <div>
+        <Header setIsSlide={setIsSlide} isSlide={isSlide} />
+        <Row className="row-layout">
+          <Col md={isSlide ? '2' : '3'} xs="4" className="col-sidebar">
+            <SideBar isSlide={isSlide} />
+          </Col>
+          <Col xs="8" md={isSlide ? '10' : '9'}>
+            <main>
+              <Container>{this.props.children}</Container>
+            </main>
+            <div>
+              <Container></Container>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
 }
 
-Layout.propTypes = {
-	children: PropTypes.node.isRequired,
-};
+type LayoutProps = {
+  children: any,
+}
 
-export default (Layout);
+export default Layout
